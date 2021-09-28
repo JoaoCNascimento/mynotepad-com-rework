e531f92316c6b4071806de446d42be181fc8055f
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-27 21:05:49 -0300

    Alteração nos imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CookieService } from 'ngx-cookie-service';
-import { AuthorizedInterceptor } from './interceptors/authorized.interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Interceptors
+import { AuthorizedInterceptor } from './interceptors/authorized.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
