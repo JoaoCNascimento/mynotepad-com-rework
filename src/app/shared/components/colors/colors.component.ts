aa0ab3608fe855bf13bb8396568bd72a90e469c6
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-27 21:05:26 -0300

    Criação de um componente para o seletor de cores do bloco de criação e edição de anotações

---
+++ src/app/shared/components/colors/colors.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-colors',
+  templateUrl: './colors.component.html',
+  styleUrls: ['./colors.component.css']
+})
+export class ColorsComponent implements OnInit {
+
+  @Output() color: EventEmitter<any> = new EventEmitter<any>();
+
+  colors = [
+    { value: "blue" },
+    { value: "yellow" },
+    { value: "red" },
+    { value: "green" },
+    { value: "pink" },
+    { value: "orange" },
+    { value: "white" },
+    { value: "black" }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  colorChange(e) {
+    // console.log(e.target.className);
+    return this.color.emit(e.target.className);
+  }
+}
