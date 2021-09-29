f80f17280d415921a9755b927c7387b2f596be42
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-29 11:33:58 -0300

    Criação e adição de guard a rotas para autenticação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'cadastrar', loadChildren: () => import("./modules/cadastrar/cadastrar.module")
-      .then(m => m.CadastrarModule)
+      .then(m => m.CadastrarModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'login', loadChildren: () => import("./modules/login/login.module")
-      .then(m => m.LoginModule)
+      .then(m => m.LoginModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'minhas-anotacoes', loadChildren: () => import("./modules/minhas-anotacoes/minhas-anotacoes.module")
