caef228afa313b9dfbb6a62475326c53fefa5a31
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-29 11:29:19 -0300

    Criação de service para login e autenticação

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,33 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  authenticated: boolean = false;
-  showMenu = new EventEmitter<boolean>();
+  isLogged = new BehaviorSubject<boolean>(false);
+
+  baseUrl = "http://localhost:8089/api/v1/user/login";
+
+  private readonly TOKEN = "token";
+
+  get token() {
+    return localStorage.getItem(this.TOKEN);
+  }
 
   constructor(
     private router: Router,
-    private cookie: CookieService
-  ) { }
-
-  login() {
-    if (this.cookie.get('jwt')) {
-      this.authenticated = true;
-      this.showMenu.emit(true);
-    }
-    else {
-      this.authenticated = false;
-      this.showMenu.emit(false);
-    }
+    private httpClient: HttpClient,
+    private toastrService: ToastrService
+  ) {
+    this.isLogged.next(!!this.token);
   }
 
-  checkAuthentication() {
-    return this.authenticated;
+  login(email: string, password: string) {
+
+    return this.httpClient.post(this.baseUrl, {
+      email, password
+    }).pipe(
+      tap((res: any) => {
+        if (res.token) {
+          this.isLogged.next(true);
+          localStorage.setItem(this.TOKEN, res.token);
+        }
+        return res;
+      }),
+      catchError(er => { this.errMessages(er); return er })
+    );
+
+  }
+
+  logout() {
+    localStorage.removeItem(this.TOKEN);
+    window.location.assign('');
+  }
+
+  // toastr messages
+  errMessages(er) {
+    console.log(er);
+    this.toastrService.error("", "Houve um erro no servidor, tente novamente mais tarde.", {
+      closeButton: true,
+      progressBar: true,
+      timeOut: 4000
+    });
   }
 }
