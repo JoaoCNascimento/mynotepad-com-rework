4948661e0d9aa3ffeef9f8d6c995dd209b2bf79b
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-29 11:31:11 -0300

    Criação de loader para requisições

---
+++ src/app/interceptors/loader.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
+import { finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+
+  constructor(
+    public loaderService: LoaderService
+  ) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.isLoading.next(true);
+
+    return next.handle(request).pipe(finalize(() => this.loaderService.hide()));
+  }
+}
+
+export const LoaderProviderInterceptor = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: LoaderInterceptor,
+  multi: true
+}
