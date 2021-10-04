309f56b437716dd095ba418154cb7c528671c7b9
João Nascimento <joao.nascimento0576@gmail.com>
2021-10-04 20:28:24 -0300

    Adição de rotas temporárias e rota para tela de manutenção

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ManutencaoComponent } from './components/manutencao/manutencao.component';
 import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: 'cadastrar', loadChildren: () => import("./modules/cadastrar/cadastrar.module")
-      .then(m => m.CadastrarModule),
-    canLoad: [AuthGuard]
+    path: 'cadastrar',
+    // loadChildren: () => import("./modules/cadastrar/cadastrar.module")
+    //   .then(m => m.CadastrarModule),
+    // canLoad: [AuthGuard],
+    redirectTo: "manutencao"
   },
   {
-    path: 'login', loadChildren: () => import("./modules/login/login.module")
-      .then(m => m.LoginModule),
-    canLoad: [AuthGuard]
+    path: 'login',
+    // loadChildren: () => import("./modules/login/login.module")
+    //   .then(m => m.LoginModule),
+    // canLoad: [AuthGuard],
+    redirectTo: "manutencao"
   },
   {
     path: 'minhas-anotacoes', loadChildren: () => import("./modules/minhas-anotacoes/minhas-anotacoes.module")
       .then(m => m.MinhasAnotacoesModule)
   },
+  {
+    path: 'manutencao', component: ManutencaoComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
 ];
 
 @NgModule({
