c90827c1bda30a8a2c8d7c93c420e6a42afb6e5e
João Nascimento <joao.nascimento0576@gmail.com>
2021-10-04 21:18:49 -0300

    Alterações para viabilizar deploy na heroky

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/mynotepad-com-rework'));
 
-app.get('/*', (req, res) => {
+app.get('/*', (req, res) =>
   res.sendFile('index.html', {
     root: 'dist/mynotepad-com-rework/'
-  })
-});
+  }),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
