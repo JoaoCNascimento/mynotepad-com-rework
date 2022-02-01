7ab2e73c10f1a8ec1bd6047cc6ae67512149fffc
JoaoCNascimento <joao.nascimento0576@gmail.com>
2022-02-01 00:04:44 -0300

    Criação de serviço de modal e atualização nos estilos globais

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  constructor() { }
+
+  revealModal() {
+    const modal: any = document.querySelectorAll(".modal-container")[0];
+    const modalBody: any = document.querySelectorAll(".modal-body")[0];
+
+    modal.style.cssText = "display: flex";
+    setTimeout(() => {
+      modalBody.style.cssText = "margin-top: 35%";
+    }, 150);
+  }
+
+  hideModal() {
+    const modal: any = document.querySelectorAll(".modal-container")[0];
+    const modalBody: any = document.querySelectorAll(".modal-body")[0];
+
+    setTimeout(() => {
+      modalBody.style.cssText = "margin-top: -105%";
+    }, 50);
+    setTimeout(() => {
+      modal.style.cssText = "display: none";
+    }, 400);
+  }
+}
