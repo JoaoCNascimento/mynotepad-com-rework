6c8fd02cb1dd27f4c85af3d62656fb2a7745e1ab
JoaoCNascimento <joao.nascimento0576@gmail.com>
2022-02-05 12:32:43 -0300

    Implementando filtros de anotações

--- src/app/modules/minhas-anotacoes/minhas-anotacoes.module.ts
+++ src/app/modules/minhas-anotacoes/minhas-anotacoes.module.ts
@@ -6,7 +6,7 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { MinhasAnotacoesComponent } from 'src/app/components/minhas-anotacoes/minhas-anotacoes.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CriarNotaComponent } from 'src/app/components/minhas-anotacoes/criar-nota/criar-nota.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditarNotaComponent } from 'src/app/components/minhas-anotacoes/editar-nota/editar-nota.component';
 import { ColorsComponent } from 'src/app/shared/components/colors/colors.component';
 import { NoteTableComponent } from 'src/app/shared/components/note-table/note-table.component';
@@ -27,7 +27,8 @@ import { AuthService } from 'src/app/services/auth.service';
     CommonModule,
     MinhasAnotacoesRoutingModule,
     FontAwesomeModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
     LocalStorageService,
