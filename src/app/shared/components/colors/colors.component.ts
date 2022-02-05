dad2ef6264f35ed0e1ebd2339adbada6f066d40a
JoaoCNascimento <joao.nascimento0576@gmail.com>
2022-02-05 12:33:19 -0300

    Adaptando componente de cores para o filtro

--- src/app/shared/components/colors/colors.component.ts
+++ src/app/shared/components/colors/colors.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-colors',
@@ -7,6 +7,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class ColorsComponent implements OnInit {
 
+  @Input() showParagraph = true;
   @Output() color: EventEmitter<any> = new EventEmitter<any>();
 
   colors = [
