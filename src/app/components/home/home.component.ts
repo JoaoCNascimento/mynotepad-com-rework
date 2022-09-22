b974f6e636fdc9f9af5fa49051a0f0a7c74f32b9
JoaoCNascimento <joao.nascimento0576@gmail.com>
2022-09-22 12:25:46 -0300

    Alterando paleta de cores e adicionando animação de imagens na homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  slideIndex = 0;
+  images = [
+    "assets/svgs/undraw_sorting_thoughts_re_fgli.svg",
+    "assets/svgs/undraw_preparation_re_t0ce.svg",
+    "assets/svgs/undraw_add_notes_re_ln36.svg"
+  ];
+  
+  currentImage = this.images[0];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.imgSlideAnimation();
+  }
+
+  imgSlideAnimation() {
+    setTimeout(() => {
+      let img = document.querySelector('img');
+      setTimeout(()=> {
+        img.classList.remove("right-slide")
+        img.classList.add("left-slide");
+        setTimeout(() => {
+          img.classList.remove("left-slide")
+          img.classList.add("right-slide");
+          this.slideIndex ++;
+          img.src = this.images[this.slideIndex];
+          
+          if(this.slideIndex == 2) 
+            this.slideIndex = -1
+          this.imgSlideAnimation();
+        }, 500);
+      });
+    }, 3000);
   }
 
 }
