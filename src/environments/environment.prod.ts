0e3971b2373867bffe9b9665a4f11082d550543c
João Nascimento <joao.nascimento0576@gmail.com>
2024-02-23 02:15:02 -0300

    Corrigindo url do backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hostUrl: 'https://mynotepad-backend-2j0cexnv4-joaocnascimento.vercel.app/api/v1/',
+  hostUrl: 'https://mynotepad-backend.vercel.app/api/v1/',
   envName: 'local'
 };
